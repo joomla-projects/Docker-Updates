aa39bc210aae3a6dc0438f8021dff8860d8cbd46
Elias Hackradt <elias@hackradt.com>
2022-03-20 12:21:06 +0100

    Added github cli with auth and clone of joom updates

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,15 @@ set -o errexit
 if [[ -n ${DEBUG:-} ]]; then
   echo "=> DEBUG is enabled"
   echo "=> Your passed args are: $1"
+  echo "=> TUF_VERSION: $TUF_VERSION"
+  echo "=> ACCESS_TOKEN: $ACCESS_TOKEN"
+  echo "=> GIT_URL: $GIT_URL"
   set -o xtrace
 fi
+
+echo "=> Setup Github CLI"
+#/usr/bin/gh
+/usr/bin/gh auth login --with-token <<< "${ACCESS_TOKEN}"
+/usr/bin/gh repo clone $GIT_URL
 
 /go/bin/tuf "$1"
